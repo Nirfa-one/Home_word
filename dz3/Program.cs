be85ad75ca8d2b06b8554a323f4f8df0979dd369
Sergey <s-kiryanov@mail.ru>
2023-01-20 15:53:25 +0300

    Добавил дз3

---
+++ dz3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int num = Convert.ToInt32(numberStr);
+
+
+for (int i = - num; i <= num; i++) {
+    if (i % 2 == 0 & i > 0) {
+       Console.WriteLine(i); 
+    }
+}
