22fb96c9f676f128abca456e547647724802254c
Sergey <s-kiryanov@mail.ru>
2023-01-20 15:52:46 +0300

    Добавил дз2

---
+++ dz2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int num = Convert.ToInt32(numberStr);
+
+
+if (num % 2 == 0) {
+    Console.WriteLine("Число четное!");
+ } else {
+   Console.WriteLine("Число не четное!"); 
+ }
