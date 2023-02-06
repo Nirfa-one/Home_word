69d387bdb4830b3f6bb3da34f76f33128b0ee027
Sergey <s-kiryanov@mail.ru>
2023-02-06 22:46:14 +0300

    Добавил дз13

---
+++ dz13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи число ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+if (stringNumber.Length > 2){
+  Console.WriteLine("Третья цифра " + stringNumber[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нет");
+}
