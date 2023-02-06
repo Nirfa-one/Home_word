10c80abd19f832bf94e9bd5791ea231143598525
Sergey <s-kiryanov@mail.ru>
2023-02-06 22:42:09 +0300

    Добавил дз10

---
+++ dz10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+Console.WriteLine("Вторая цифра числа " + stringNumber[1]);
