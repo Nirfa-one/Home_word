7094258601dcf3617e59b1aadc972ffb408f0251
Sergey <s-kiryanov@mail.ru>
2023-02-06 22:55:23 +0300

    Добавил дз15

---
+++ dz15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи цифру, обозначающую день недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int number) {
+  if (number == 6 || number == 7) {
+  Console.WriteLine("Этот день выходной!");
+  }
+  else if (number < 1 || number > 7) {
+    Console.WriteLine("Такого дня не существует!");
+  }
+  else Console.WriteLine("Это будний день!");
+}
+
+CheckingTheDayOfTheWeek(number);
